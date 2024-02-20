add43d59e1416aabe9594b5fb837ef58021aaa9b
AngelChenAnChi <angelhaniel@icloud.com>
2024-02-20 17:34:35 +0800

    加入專案檔案。

---
+++ BrickBuster_Game/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace Brick_Buster_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_level = new System.Windows.Forms.Label();
+            this.label_score = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox_life3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_life2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_life = new System.Windows.Forms.PictureBox();
+            this.pictureBox_bar = new System.Windows.Forms.PictureBox();
+            this.pictureBox_ball = new System.Windows.Forms.PictureBox();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.button_start = new System.Windows.Forms.Button();
+            this.label_count = new System.Windows.Forms.Label();
+            this.label_instruction = new System.Windows.Forms.Label();
+            this.pictureBox_brick = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer_countDown = new System.Windows.Forms.Timer(this.components);
+            this.label_over = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_life3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_life2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_life)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_bar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_brick)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_level
+            // 
+            this.label_level.AutoSize = true;
+            this.label_level.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_level.ForeColor = System.Drawing.Color.White;
+            this.label_level.Location = new System.Drawing.Point(12, 9);
+            this.label_level.Name = "label_level";
+            this.label_level.Size = new System.Drawing.Size(117, 38);
+            this.label_level.TabIndex = 0;
+            this.label_level.Text = "label1";
+            // 
+            // label_score
+            // 
+            this.label_score.AutoSize = true;
+            this.label_score.BackColor = System.Drawing.SystemColors.InfoText;
+            this.label_score.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_score.ForeColor = System.Drawing.Color.Gold;
+            this.label_score.Location = new System.Drawing.Point(567, 10);
+            this.label_score.Name = "label_score";
+            this.label_score.Size = new System.Drawing.Size(147, 38);
+            this.label_score.TabIndex = 1;
+            this.label_score.Text = "SCORE:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(947, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Life:";
+            // 
+            // pictureBox_life3
+            // 
+            this.pictureBox_life3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_life3.Image")));
+            this.pictureBox_life3.Location = new System.Drawing.Point(1046, 9);
+            this.pictureBox_life3.Name = "pictureBox_life3";
+            this.pictureBox_life3.Size = new System.Drawing.Size(46, 40);
+            this.pictureBox_life3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_life3.TabIndex = 3;
+            this.pictureBox_life3.TabStop = false;
+            // 
+            // pictureBox_life2
+            // 
+            this.pictureBox_life2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_life2.Image")));
+            this.pictureBox_life2.Location = new System.Drawing.Point(1112, 8);
+            this.pictureBox_life2.Name = "pictureBox_life2";
+            this.pictureBox_life2.Size = new System.Drawing.Size(46, 40);
+            this.pictureBox_life2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_life2.TabIndex = 4;
+            this.pictureBox_life2.TabStop = false;
+            // 
+            // pictureBox_life
+            // 
+            this.pictureBox_life.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_life.Image")));
+            this.pictureBox_life.Location = new System.Drawing.Point(1179, 8);
+            this.pictureBox_life.Name = "pictureBox_life";
+            this.pictureBox_life.Size = new System.Drawing.Size(46, 40);
+            this.pictureBox_life.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_life.TabIndex = 5;
+            this.pictureBox_life.TabStop = false;
+            // 
+            // pictureBox_bar
+            // 
+            this.pictureBox_bar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_bar.Image")));
+            this.pictureBox_bar.Location = new System.Drawing.Point(549, 609);
+            this.pictureBox_bar.Name = "pictureBox_bar";
+            this.pictureBox_bar.Size = new System.Drawing.Size(205, 31);
+            this.pictureBox_bar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_bar.TabIndex = 6;
+            this.pictureBox_bar.TabStop = false;
+            // 
+            // pictureBox_ball
+            // 
+            this.pictureBox_ball.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_ball.Image")));
+            this.pictureBox_ball.Location = new System.Drawing.Point(617, 441);
+            this.pictureBox_ball.Name = "pictureBox_ball";
+            this.pictureBox_ball.Size = new System.Drawing.Size(58, 52);
+            this.pictureBox_ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_ball.TabIndex = 7;
+            this.pictureBox_ball.TabStop = false;
+            // 
+            // button_exit
+            // 
+            this.button_exit.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button_exit.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_exit.Location = new System.Drawing.Point(544, 209);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(210, 50);
+            this.button_exit.TabIndex = 8;
+            this.button_exit.Text = "EXIT";
+            this.button_exit.UseVisualStyleBackColor = false;
+            this.button_exit.Visible = false;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // button_start
+            // 
+            this.button_start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button_start.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_start.Location = new System.Drawing.Point(544, 282);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(210, 50);
+            this.button_start.TabIndex = 9;
+            this.button_start.Text = "START";
+            this.button_start.UseVisualStyleBackColor = false;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // label_count
+            // 
+            this.label_count.AutoSize = true;
+            this.label_count.Font = new System.Drawing.Font("Cooper Black", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_count.ForeColor = System.Drawing.Color.White;
+            this.label_count.Location = new System.Drawing.Point(931, 242);
+            this.label_count.Name = "label_count";
+            this.label_count.Size = new System.Drawing.Size(130, 137);
+            this.label_count.TabIndex = 10;
+            this.label_count.Text = "3";
+            this.label_count.Visible = false;
+            // 
+            // label_instruction
+            // 
+            this.label_instruction.AutoSize = true;
+            this.label_instruction.BackColor = System.Drawing.Color.GreenYellow;
+            this.label_instruction.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_instruction.Location = new System.Drawing.Point(316, 354);
+            this.label_instruction.Name = "label_instruction";
+            this.label_instruction.Size = new System.Drawing.Size(669, 128);
+            this.label_instruction.TabIndex = 11;
+            this.label_instruction.Text = "-> : right     <- : left  \r\nEnter : start    Space : pause   Esc : exit\r\nWhen you" +
+    " break one brick, you will get 5 points.\r\nWhen you miss the ball, you will loose" +
+    " 10 points.\r\n";
+            this.label_instruction.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox_brick
+            // 
+            this.pictureBox_brick.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_brick.Image")));
+            this.pictureBox_brick.Location = new System.Drawing.Point(71, 107);
+            this.pictureBox_brick.Name = "pictureBox_brick";
+            this.pictureBox_brick.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox_brick.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_brick.TabIndex = 12;
+            this.pictureBox_brick.TabStop = false;
+            this.pictureBox_brick.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer_countDown
+            // 
+            this.timer_countDown.Interval = 1000;
+            this.timer_countDown.Tick += new System.EventHandler(this.timer_countDown_Tick);
+            // 
+            // label_over
+            // 
+            this.label_over.AutoSize = true;
+            this.label_over.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label_over.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_over.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label_over.Location = new System.Drawing.Point(537, 354);
+            this.label_over.Name = "label_over";
+            this.label_over.Size = new System.Drawing.Size(231, 38);
+            this.label_over.TabIndex = 13;
+            this.label_over.Text = "GAME OVER";
+            this.label_over.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1250, 652);
+            this.Controls.Add(this.label_over);
+            this.Controls.Add(this.pictureBox_brick);
+            this.Controls.Add(this.label_instruction);
+            this.Controls.Add(this.label_count);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.pictureBox_ball);
+            this.Controls.Add(this.pictureBox_bar);
+            this.Controls.Add(this.pictureBox_life);
+            this.Controls.Add(this.pictureBox_life2);
+            this.Controls.Add(this.pictureBox_life3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_score);
+            this.Controls.Add(this.label_level);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Brick Buster Game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_life3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_life2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_life)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_bar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_brick)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_level;
+        private System.Windows.Forms.Label label_score;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox_life3;
+        private System.Windows.Forms.PictureBox pictureBox_life2;
+        private System.Windows.Forms.PictureBox pictureBox_life;
+        private System.Windows.Forms.PictureBox pictureBox_bar;
+        private System.Windows.Forms.PictureBox pictureBox_ball;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Label label_count;
+        private System.Windows.Forms.Label label_instruction;
+        private System.Windows.Forms.PictureBox pictureBox_brick;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer_countDown;
+        private System.Windows.Forms.Label label_over;
+    }
+}
+
---
+++ BrickBuster_Game/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Reflection;
+namespace Brick_Buster_Game
+{
+    public partial class Form1 : Form
+    {
+        int brickRow = 3;
+        int brickCol = 7;
+        PictureBox[,] bricks;
+        int xSpeed = 30;
+        int ySpeed = 30;
+        int life = 3;
+        int level = 1;
+        int score = 0;
+        int getPoint = 5;
+        int loosePoint = -10;
+        bool allGone = false;
+        int count = 3;
+        bool tcd = false;
+        bool t1 = false;
+ 
+        //bricks on winform
+        void StartGame()
+        {
+            LifeLevel();
+            // set the number of bricks in different level
+            if (level == 1)
+            {
+                brickRow = 3;
+                brickCol = 7;
+            }
+            else if (level == 2)
+            {
+                brickRow = 5;
+                brickCol = 9;
+                xSpeed = xSpeed + 5;
+                ySpeed = xSpeed + 5;
+            }
+            else if (level == 3)
+            {
+                brickRow = 7;
+                brickCol = 11;
+                xSpeed = xSpeed + 10;
+                ySpeed = ySpeed + 10;
+            }
+
+            //design bricks on the form
+            bricks = new PictureBox[brickRow, brickCol];
+            int w, h = 40;
+            w = ClientRectangle.Width / brickCol;
+
+            for (int r = 0; r < brickRow; r++)
+            {
+                for (int c = 0; c < brickCol; c++)
+                {
+                    bricks[r, c] = new PictureBox();
+                    bricks[r, c].Width = w;
+                    bricks[r, c].Height = h;
+                    bricks[r, c].Top = 60 + r * h;
+                    bricks[r, c].Left = w * c;
+                    bricks[r, c].SizeMode = PictureBoxSizeMode.StretchImage;
+                    bricks[r, c].Image = pictureBox_brick.Image;
+                    Controls.Add(bricks[r, c]);
+                }
+            }
+
+        }
+        void LifeLevel()
+        {
+            label_level.Text = $"Level : {level}";
+            if (life == 3)
+            {
+                pictureBox_life.Visible = true;
+            }
+            else if (life == 2)
+            {
+                pictureBox_life.Visible = false;
+            }
+            else if (life == 1)
+            {
+                pictureBox_life2.Visible = false;
+            }
+            else if (life == 0)
+            {
+                pictureBox_life3.Visible = false;
+            }
+        }
+        public string showScore()
+        {
+            return label_score.Text = $"SCORE: {score}";
+        }
+        void positionSet()
+        {
+            // postion of the start button
+            button_start.Left = ClientRectangle.Width / 2 - button_start.Width / 2;
+            button_start.Top = ClientRectangle.Height / 2 - button_start.Height;
+            // position of the exit button
+            button_exit.Left = ClientRectangle.Width / 2 - button_exit.Width / 2;
+            button_exit.Top = ClientRectangle.Height / 2 - button_exit.Height;
+            //postiion of the instruction
+            label_instruction.Left = ClientRectangle.Width / 2 - label_instruction.Width / 2;
+            label_instruction.Top = ClientRectangle.Height / 2 + label_instruction.Height / 2;
+            //postion of the over
+            label_over.Left = ClientRectangle.Width / 2 - label_over.Width / 2;
+            label_over.Top = ClientRectangle.Height / 2 + label_over.Height;
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            positionSet();
+            StartGame();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+            //movement of the ball
+            pictureBox_ball.Left = pictureBox_ball.Left + xSpeed;
+            pictureBox_ball.Top = pictureBox_ball.Top + ySpeed;
+
+            //rebound (left and right)
+            if (pictureBox_ball.Bounds.Right >= ClientRectangle.Width || pictureBox_ball.Bounds.Left <= 0)
+            {
+                //soundCollision.Play();
+                xSpeed = -xSpeed;
+            }
+            //rebound (top and buttom)
+            if (pictureBox_ball.Bounds.Top <= 50 || (pictureBox_ball.Bottom < pictureBox_bar.Bottom && pictureBox_ball.Bounds.IntersectsWith(pictureBox_bar.Bounds)))
+            {
+                //soundCollision.Play();
+                ySpeed = -ySpeed;
+            }
+
+            allGone = true;
+            // collision (ball and bricks)
+            foreach (PictureBox b in bricks)
+            {
+                if (b.Visible)
+                {
+                    if (pictureBox_ball.Bounds.IntersectsWith(b.Bounds))
+                    {
+                        b.Visible = false;
+                        score = score + getPoint;                    
+                    }
+                }
+                if (b.Visible) allGone = false;
+            }
+
+
+            // all bricks are disappeared
+            if (allGone)
+            {           
+                timer1.Enabled = false;
+                //up level
+                level = level + 1;
+                //next level, button show up
+                if (level <= 3)
+                {
+                    button_start.Visible = true;
+                    pictureBox_ball.Left = ClientRectangle.Width / 2 - pictureBox_ball.Width / 2;
+                    pictureBox_ball.Top = ClientRectangle.Height / 2 + pictureBox_ball.Height * 4;
+                    pictureBox_bar.Left = ClientRectangle.Width / 2 - pictureBox_bar.Width / 2;
+                    pictureBox_bar.Top = ClientRectangle.Height - pictureBox_bar.Height;
+                }
+                else
+                {
+                    button_exit.Visible = true;
+                    label_over.Visible = true;
+                    pictureBox_ball.Visible = false;
+                    pictureBox_bar.Visible = false;
+                }
+
+            }
+
+            // the ball miss the bar
+            if (pictureBox_ball.Top > ClientRectangle.Height)
+            {
+                score = score + loosePoint;
+                timer1.Enabled = false;
+                life = life - 1;
+                if (life > 0)
+                {
+                    LifeLevel();
+                    button_start.Visible = true;
+                    pictureBox_ball.Left = ClientRectangle.Width / 2 - pictureBox_ball.Width / 2;
+                    pictureBox_ball.Top = ClientRectangle.Height / 2 + pictureBox_ball.Height * 4;
+                    pictureBox_bar.Left = ClientRectangle.Width / 2 - pictureBox_bar.Width / 2;
+                    pictureBox_bar.Top = ClientRectangle.Height - pictureBox_bar.Height;
+                }
+                else
+                {
+                    LifeLevel();
+                    button_exit.Visible = true;
+                    label_over.Visible = true;
+                    pictureBox_ball.Visible = false;
+                    pictureBox_bar.Visible = false;
+                }
+            }
+            showScore();
+        }
+
+        private void timer_countDown_Tick(object sender, EventArgs e)
+        {
+            timer_countDown.Interval = 1000;
+            // show the ball and the bar
+            pictureBox_ball.Left = ClientRectangle.Width / 2 - pictureBox_ball.Width / 2;
+            pictureBox_ball.Top = ClientRectangle.Height / 2 + pictureBox_ball.Height * 4;
+            pictureBox_bar.Left = ClientRectangle.Width / 2 - pictureBox_bar.Width / 2;
+            pictureBox_bar.Top = ClientRectangle.Height - pictureBox_bar.Height;
+            pictureBox_ball.Visible = true;
+            pictureBox_bar.Visible = true;
+            //3,2,1
+            label_count.Left = ClientRectangle.Width / 2 - label_count.Width / 2;
+            label_count.Top = ClientRectangle.Height / 2;
+            if (count > 0)
+            {
+                label_count.Visible = true;
+                label_count.Text = count--.ToString();
+            }
+            else
+            {
+                label_count.Visible = false;
+                // the ball start to move
+                timer_countDown.Stop();
+                timer1.Enabled = true;
+                count = 3;
+            }
+        }
+
+        // button
+        // start
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            timer_countDown.Enabled = true;
+            button_start.Visible = false;
+            label_instruction.Visible = false;
+            if (allGone)
+            {
+                StartGame();
+                allGone = false;
+            }
+        }
+        //exit
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            if (label_over.Visible)
+            {
+                this.Close();
+            }
+        }
+
+        // keyboard function
+        // left,right,enter,space,esc
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            int x = pictureBox_bar.Location.X;
+            int y = pictureBox_bar.Location.Y;
+
+            //capture left arrow key
+            if (keyData == Keys.Left)
+            {
+                if (pictureBox_bar.Bounds.Left >= 0) x -= 110;
+                Point position = new Point(x, y);
+                pictureBox_bar.Location = position;
+                return true;
+            }
+            //capture right arrow key
+            if (keyData == Keys.Right)
+            {
+                if (pictureBox_bar.Bounds.Right <= ClientRectangle.Width) x += 110;
+                Point position = new Point(x, y);
+                pictureBox_bar.Location = position;
+                return true;
+            }
+            //capture enter key
+            if (keyData == Keys.Enter)
+            {
+                this.button_start.PerformClick();
+                return true;
+            }
+            //capture space key
+            if (keyData == Keys.Space)
+            {
+                if (timer_countDown.Enabled && !timer1.Enabled)
+                {
+                    timer_countDown.Enabled = !timer_countDown.Enabled;
+                    tcd = true;
+                }
+                else if (!timer_countDown.Enabled && timer1.Enabled)
+                {
+                    timer1.Enabled = !timer1.Enabled;
+                    t1 = true;
+                }
+                else
+                {
+                    if (tcd)
+                    {
+                        timer_countDown.Enabled = !timer_countDown.Enabled;
+                        tcd = !tcd;
+                    }
+                    if (t1)
+                    {
+                        timer1.Enabled = !timer1.Enabled;
+                        t1 = !t1;
+                    }
+                }
+                return true;
+            }
+            //capture esc key
+            if (keyData == Keys.Escape)
+            {
+                this.button_exit.PerformClick();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+    }
+}
